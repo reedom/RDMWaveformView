7e80b6c55d8fb24d15eb4e849755e4f42688a4db
HANAI tohru <tohru@reedom.com>
2019-04-16 16:52:50 +0900

    feat: implement demo view

--- Example/RDMWaveformView/ViewController.swift
+++ Example/RDMWaveformView/ViewController.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import RDMWaveformView
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  var waveformView: RDMWaveformView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    waveformView = RDMWaveformView()
+    view.addSubview(waveformView)
+
+    waveformView.translatesAutoresizingMaskIntoConstraints = false
+    waveformView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    waveformView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+    waveformView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+    waveformView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    waveformView.backgroundColor = UIColor.red
+
+    let url = Bundle.main.url(forResource: "file_example_MP3_700KB", withExtension: "mp3")
+    waveformView.audioURL = url
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
 }
-
